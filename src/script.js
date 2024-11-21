f42615b2052abd3d50472497ac1456526975d7c3
EvandroKaibara <yujievandro@gmail.com>
2024-11-21 19:41:13 -0300

    HTML e CSS finalizados

--- src/script.js
+++ src/script.js
@@ -6,13 +6,13 @@ const cardWidth = document.querySelector('.post--card').offsetWidth;
 let currentScroll = 0;
 preBtn.addEventListener('click', () => {
     currentScroll -= cardWidth * 4; 
-    if (currentScroll < 0) currentScroll = 1;
+    if (currentScroll < 0) currentScroll = 3;
     posts.style.transform = `translateX(-${currentScroll}px)`;
 });
 
 nxtBtn.addEventListener('click', () => {
     const maxScroll = posts.scrollWidth - posts.offsetWidth;
-    currentScroll += cardWidth * 1; 
+    currentScroll += cardWidth * 3; 
     if (currentScroll > maxScroll) currentScroll = maxScroll;
     posts.style.transform = `translateX(-${currentScroll}px)`;
 });
